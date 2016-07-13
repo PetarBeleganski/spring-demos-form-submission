faece4ee93a07a8c92a4bc33d79d9e041692737b
Petar Beleganski <petar.beleganski@scalefocus.com>
2016-07-13 16:53:00 +0300

    Finished form-submission demo using spring mvc

---
+++ src/main/java/user/User.java
@@ -0,0 +1,25 @@
+package user;
+
+/**
+ * Created by petar.beleganski on 13.7.2016 г..
+ */
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
